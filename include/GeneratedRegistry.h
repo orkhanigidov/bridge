fb68c1b2555bf72c4dbaed5d84708e35f5ca3596
oku <iorxan1995@gmail.com>
2025-04-09 19:48:00 +0200

    feat: add GeneratedRegistry header for object and method registration

---
+++ include/GeneratedRegistry.h
@@ -0,0 +1,12 @@
+#ifndef GENERATEDREGISTRY_H
+#define GENERATEDREGISTRY_H
+
+#include "Registry.h"
+
+namespace generated {
+    void registerObjects(Registry *registry);
+
+    void registerAllMethods(Registry *registry);
+}
+
+#endif //GENERATEDREGISTRY_H
