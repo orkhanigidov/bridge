b78e330f306dbd7fa89d2f27015268585079e799
oku <iorxan1995@gmail.com>
2025-07-02 17:18:33 +0200

    refactor: rename and restructure DTOs

---
+++ include/engine/dto/execution_request.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "pipeline.hpp"
+
+#include <oatpp/core/Types.hpp>
+#include <oatpp/core/macro/codegen.hpp>
+
+namespace engine::dto
+{
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+    class execution_request final : public oatpp::DTO
+    {
+        DTO_INIT(execution_request, DTO)
+
+        DTO_FIELD_INFO(type)
+        {
+            info->description =
+                "Type of execution to perform, e.g., 'lua script' or 'json pipeline'";
+            info->required = true;
+        }
+        DTO_FIELD(String, type);
+
+        DTO_FIELD_INFO(pipelines)
+        {
+            info->description = "Collection of pipeline definitions to process";
+            info->required    = false;
+        }
+        DTO_FIELD(List<pipeline>, pipelines);
+
+        DTO_FIELD_INFO(script_path)
+        {
+            info->description = "File path to the executable script (native mode)";
+            info->required    = false;
+        }
+        DTO_FIELD(String, script_path);
+
+        DTO_FIELD_INFO(script_content)
+        {
+            info->description = "Content of the script to execute (network mode)";
+            info->required    = false;
+        }
+        DTO_FIELD(String, script_content);
+    };
+
+#include OATPP_CODEGEN_END(DTO)
+} // namespace engine::dto
---
+++ include/engine/dto/pipeline_step.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <oatpp/core/Types.hpp>
+#include <oatpp/core/macro/codegen.hpp>
+
+namespace engine::dto
+{
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+    class pipeline_step final : public oatpp::DTO
+    {
+        DTO_INIT(pipeline_step, DTO)
+
+        DTO_FIELD_INFO(class_name)
+        {
+            info->description = "Class name for object instantiation";
+            info->required    = false;
+        }
+        DTO_FIELD(String, class_name, "create");
+
+        DTO_FIELD_INFO(alias)
+        {
+            info->description = "Variable name reference for the instantiated object";
+            info->required    = false;
+        }
+        DTO_FIELD(String, alias, "as");
+
+        DTO_FIELD_INFO(function_name)
+        {
+            info->description = "Function or method name to call";
+            info->required    = false;
+        }
+        DTO_FIELD(String, function_name, "call");
+
+        DTO_FIELD_INFO(constructor_args)
+        {
+            info->description = "Arguments passed to constructors";
+            info->required    = false;
+        }
+        DTO_FIELD(List<Any>, constructor_args, "with");
+
+        DTO_FIELD_INFO(function_args)
+        {
+            info->description = "Arguments passed to functions or methods";
+            info->required    = false;
+        }
+        DTO_FIELD(List<Any>, function_args, "using");
+    };
+
+#include OATPP_CODEGEN_END(DTO)
+} // namespace engine::dto
