614c6852d9cbd21bee42f51793941b5da5852f30
Orkhan Igidov <iorxan1995@gmail.com>
2025-07-18 13:38:03 +0200

    feat: replace HeaderIncluder with HeaderCollector for improved header file processing

---
+++ include/codegen/header_collector.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace fs = std::filesystem;
+
+namespace codegen
+{
+    class HeaderCollector final
+    {
+    public:
+        HeaderCollector() = default;
+
+        bool collect_headers(const fs::path& include_dir, const fs::path& output_file);
+
+    private:
+        bool is_header_file(const fs::path& path);
+    };
+} // namespace codegen
