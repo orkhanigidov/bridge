0cf297ec6bb6a3d52a3bb46657ea172a066e3ea9
Orkhan Igidov <iorxan1995@gmail.com>
2025-09-22 00:40:11 +0200

    refactor: expand standard library includes in `pch.hpp` and add `fs` namespace alias

--- include/pch.hpp
+++ include/pch.hpp
@@ -1,14 +1,24 @@
 #pragma once
 
 // Standard library headers
+#include <chrono>
+#include <cstdint>
 #include <filesystem>
+#include <format>
+#include <fstream>
+#include <iosfwd>
+#include <iostream>
 #include <memory>
 #include <optional>
+#include <ranges>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 #include <unordered_map>
+#include <unordered_set>
+#include <utility>
 #include <variant>
 #include <vector>
 
-// 3rd party library headers
-#include <sol/sol.hpp>
+// Namespace aliases
+namespace fs = std::filesystem;
