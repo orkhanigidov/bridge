bdd0dfa99be2cab531fd08d2df21fd382cb5fe36
Orkhan Igidov <iorxan1995@gmail.com>
2025-10-18 14:06:16 +0200

    refactor: enhance `to_lua_table` function overloads to support both mapped and non-mapped container types

--- include/bindings/lua/utils/lua_utils.hpp
+++ include/bindings/lua/utils/lua_utils.hpp
@@ -4,14 +4,36 @@
 
 namespace engine::bindings::lua::utils
 {
-    template <typename Container>
-    sol::table to_lua_table(sol::state& lua, const Container& container)
+    template <typename T, typename = void>
+    struct is_mapped_type : std::false_type
+    {
+    };
+
+    template <typename T>
+    struct is_mapped_type<T, std::void_t<typename T::key_type, typename T::mapped_type>> : std::true_type
+    {
+    };
+
+    template <typename Container, std::enable_if_t<!is_mapped_type<Container>::value, int>  = 0>
+    sol::table to_lua_table(sol::state_view lua, const Container& container)
     {
         sol::table table = lua.create_table_with(container.size(), 0);
 
         for (const auto& elem : container)
         {
             table.add(elem);
+        }
+        return table;
+    }
+
+    template <typename Container, std::enable_if_t<is_mapped_type<Container>::value, int>  = 0>
+    sol::table to_lua_table(sol::state_view lua, const Container& container)
+    {
+        sol::table table = lua.create_table_with(0, container.size());
+
+        for (const auto& [key, value] : container)
+        {
+            table[key] = value;
         }
         return table;
     }
