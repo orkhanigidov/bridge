d2003ddc0a9a596e11549a5651f0a124c6c86590
Orkhan Igidov <iorxan1995@gmail.com>
2025-10-20 14:52:24 +0200

    feat: add detailed documentation for metadata descriptors (Constructor, Function, Class)

--- tools/codegen/include/metadata/class_descriptor.hpp
+++ tools/codegen/include/metadata/class_descriptor.hpp
@@ -7,75 +7,143 @@
 
 namespace codegen::metadata
 {
+    /**
+     * @brief Represents metadata for a class in the code generation system.
+     *
+     * This class holds information about a class's name, base classes, constructors,
+     * member enumerators, variables, and functions. It provides methods to query
+     * and modify this metadata in a fluent interface style.
+     */
     class ClassDescriptor final
     {
     public:
+        /**
+         * @brief Default constructor.
+         */
         ClassDescriptor() = default;
 
+        /**
+         * @brief Constructs a ClassDescriptor with the given name.
+         * @param name The name of the class.
+         */
         explicit ClassDescriptor(std::string name): name_(std::move(name))
         {
         }
 
+        /**
+         * @brief Gets the name of the class.
+         * @return A constant reference to the class name.
+         */
         const std::string& name() const noexcept
         {
             return name_;
         }
 
+        /**
+         * @brief Gets the list of base class names.
+         * @return A constant reference to the vector of base class names.
+         */
         const std::vector<std::string>& base_class_names() const noexcept
         {
             return base_class_names_;
         }
 
+        /**
+         * @brief Gets the list of constructors.
+         * @return A constant reference to the vector of ConstructorDescriptors.
+         */
         const std::vector<ConstructorDescriptor>& constructors() const noexcept
         {
             return constructors_;
         }
 
+        /**
+         * @brief Gets the list of member enumerators.
+         * @return A constant reference to the vector of EnumeratorDescriptors.
+         */
         const std::vector<EnumeratorDescriptor>& member_enums() const noexcept
         {
             return member_enums_;
         }
 
+        /**
+         * @brief Gets the list of member variables.
+         * @return A constant reference to the vector of VariableDescriptors.
+         */
         const std::vector<VariableDescriptor>& member_variables() const noexcept
         {
             return member_variables_;
         }
 
+        /**
+         * @brief Gets the list of member functions.
+         * @return A constant reference to the vector of FunctionDescriptors.
+         */
         const std::vector<FunctionDescriptor>& member_functions() const noexcept
         {
             return member_functions_;
         }
 
+        /**
+         * @brief Sets the name of the class.
+         * @param name The new name of the class.
+         * @return A reference to this ClassDescriptor for chaining.
+         */
         ClassDescriptor& set_name(std::string name)
         {
             name_ = std::move(name);
             return *this;
         }
 
+        /**
+         * @brief Adds a base class name.
+         * @param base_class_name The name of the base class to add.
+         * @return A reference to this ClassDescriptor for chaining.
+         */
         ClassDescriptor& add_base_class_name(std::string base_class_name)
         {
             base_class_names_.emplace_back(std::move(base_class_name));
             return *this;
         }
 
+        /**
+         * @brief Adds a constructor descriptor.
+         * @param constructor The ConstructorDescriptor to add.
+         * @return A reference to this ClassDescriptor for chaining.
+         */
         ClassDescriptor& add_constructor(ConstructorDescriptor constructor)
         {
             constructors_.emplace_back(std::move(constructor));
             return *this;
         }
 
+        /**
+         * @brief Adds a member enumerator descriptor.
+         * @param enumerator The EnumeratorDescriptor to add.
+         * @return A reference to this ClassDescriptor for chaining.
+         */
         ClassDescriptor& add_member_enumerator(EnumeratorDescriptor enumerator)
         {
             member_enums_.emplace_back(std::move(enumerator));
             return *this;
         }
 
+        /**
+         * @brief Adds a member variable descriptor.
+         * @param member_variable The VariableDescriptor to add.
+         * @return A reference to this ClassDescriptor for chaining.
+         */
         ClassDescriptor& add_member_variable(VariableDescriptor member_variable)
         {
             member_variables_.emplace_back(std::move(member_variable));
             return *this;
         }
 
+        /**
+         * @brief Adds a member function descriptor.
+         * @param member_function The FunctionDescriptor to add.
+         * @return A reference to this ClassDescriptor for chaining.
+         */
         ClassDescriptor& add_member_function(FunctionDescriptor member_function)
         {
             member_functions_.emplace_back(std::move(member_function));
@@ -83,11 +151,11 @@ namespace codegen::metadata
         }
 
     private:
-        std::string name_;
-        std::vector<std::string> base_class_names_;
-        std::vector<ConstructorDescriptor> constructors_;
-        std::vector<EnumeratorDescriptor> member_enums_;
-        std::vector<VariableDescriptor> member_variables_;
-        std::vector<FunctionDescriptor> member_functions_;
+        std::string name_; /**< The name of the class. */
+        std::vector<std::string> base_class_names_; /**< List of base class names. */
+        std::vector<ConstructorDescriptor> constructors_; /**< List of constructor descriptors. */
+        std::vector<EnumeratorDescriptor> member_enums_; /**< List of member enumerator descriptors. */
+        std::vector<VariableDescriptor> member_variables_; /**< List of member variable descriptors. */
+        std::vector<FunctionDescriptor> member_functions_; /**< List of member function descriptors. */
     };
 } // namespace codegen::metadata
--- tools/codegen/include/metadata/constructor_descriptor.hpp
+++ tools/codegen/include/metadata/constructor_descriptor.hpp
@@ -4,42 +4,83 @@
 
 namespace codegen::metadata
 {
+    /**
+     * @brief Represents metadata for a constructor in the code generation system.
+     *
+     * This class holds information about a constructor's class name, parameters,
+     * and signature. It provides methods to query and modify this metadata
+     * in a fluent interface style.
+     */
     class ConstructorDescriptor final
     {
     public:
+        /**
+         * @brief Default constructor.
+         */
         ConstructorDescriptor() = default;
 
+        /**
+         * @brief Constructs a ConstructorDescriptor with the given class name.
+         * @param class_name The name of the class the constructor belongs to.
+         */
         explicit ConstructorDescriptor(std::string class_name): class_name_(std::move(class_name))
         {
         }
 
+        /**
+         * @brief Gets the name of the class.
+         * @return A constant reference to the class name.
+         */
         const std::string& class_name() const noexcept
         {
             return class_name_;
         }
 
+        /**
+         * @brief Gets the list of parameters.
+         * @return A constant reference to the vector of ParameterDescriptors.
+         */
         const std::vector<ParameterDescriptor>& parameters() const noexcept
         {
             return parameters_;
         }
 
+        /**
+         * @brief Gets the signature of the constructor.
+         * @return A constant reference to the constructor signature.
+         */
         const std::string& signature() const noexcept
         {
             return signature_;
         }
 
+        /**
+         * @brief Sets the name of the class.
+         * @param class_name The new name of the class.
+         * @return A reference to this ConstructorDescriptor for chaining.
+         */
         ConstructorDescriptor& set_class_name(std::string class_name)
         {
             class_name_ = std::move(class_name);
             return *this;
         }
 
+        /**
+         * @brief Adds a parameter descriptor.
+         * @param parameter The ParameterDescriptor to add.
+         * @return A reference to this ConstructorDescriptor for chaining.
+         */
         ConstructorDescriptor& add_parameter(ParameterDescriptor parameter)
         {
             parameters_.emplace_back(std::move(parameter));
             return *this;
         }
 
+        /**
+         * @brief Sets the signature of the constructor.
+         * @param signature The signature of the constructor to set.
+         * @return A reference to this ConstructorDescriptor for chaining.
+         */
         ConstructorDescriptor& set_signature(std::string signature)
         {
             signature_ = std::move(signature);
@@ -47,8 +88,8 @@ namespace codegen::metadata
         }
 
     private:
-        std::string class_name_;
-        std::vector<ParameterDescriptor> parameters_;
-        std::string signature_;
+        std::string class_name_; /**< The name of the class the constructor belongs to. */
+        std::vector<ParameterDescriptor> parameters_; /**< List of parameter descriptors. */
+        std::string signature_; /**< The signature of the constructor. */
     };
 } // namespace codegen::metadata
