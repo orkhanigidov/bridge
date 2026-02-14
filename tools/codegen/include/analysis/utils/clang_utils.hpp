730fc37019e8be46f387e710e48af7ad0f69d0bc
Orkhan Igidov <iorxan1995@gmail.com>
2026-02-14 02:40:43 +0100

    chore: update license headers from GPL to AGPLv3

--- tools/codegen/include/analysis/utils/clang_utils.hpp
+++ tools/codegen/include/analysis/utils/clang_utils.hpp
@@ -7,10 +7,10 @@
  * Developed as part of the master's thesis at the University of Konstanz.
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, version 3.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
